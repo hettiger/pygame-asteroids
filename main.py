38c66b50fde4a9c635eadfc051d2168502bfca74
Martin Hettiger <martin@hettiger.com>
2025-03-29 21:43:10 +0100

    Limit to 60 FPS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+
 from constants import *
 
 def main():
@@ -10,12 +11,15 @@ def main():
     )
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1_000
 
 if __name__ == "__main__":
     main()
