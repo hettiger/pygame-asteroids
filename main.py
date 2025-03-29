6ddc2fe5a6a9180356f417e6f7e750249f4af42a
Martin Hettiger <martin@hettiger.com>
2025-03-29 15:40:51 +0100

    Scaffold game loop

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def main():
         f"Screen height: {SCREEN_HEIGHT}",
         sep="\n"
     )
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color=(0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
